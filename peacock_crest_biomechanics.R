b7d3bed2143c82f61868545904c0edf0fddb17fd
rozdakin <roslyn.dakin@gmail.com>
2017-08-15 15:52:35 -0400

    modified shaking data on plot of f_res and Q vibration results

--- peacock_crest_biomechanics.R
+++ peacock_crest_biomechanics.R
@@ -81,9 +81,13 @@ mod.f.rep <- lme(f_res ~ 1, random=~1|crest_number, data=subset(vib, whole_singl
 r.squaredGLMM(mod.f.rep) # 82% measurement repeatability, very high
 
 
-# plot data and model fits for fr and Q
+# plot data and some model fits for both fr and Q, for Figure 2 of manuscript
+# add:
+# male mean display freq = 25.6, range of bird means 23.9-27.1; range 22.2 to 28.2
+# female mean display freq = 26.1, range of bird means 25.2-27.1; range 25.2 to 28.5
 
-dev.new(width=7, height=2)
+png(file='./figures/fig2_vib_results.png', width=7, height=2, res=300, units='in', bg='white')
+
 par(mfrow=c(2, 6), bty='l', las=1, mar=c(3,3,0.25,0.25), mgp=c(1.5,0.5,0), cex.axis=0.5, cex.lab=0.5, tck=-0.05)
 
 plot(f_res ~ jitter(as.numeric(orientation), 0.5), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='f (Hz)', xlab='orientation', xaxt='n', xlim=c(0.5,2.5), ylim=c(19,33), cex=0.5, yaxt='n')
@@ -92,16 +96,16 @@ axis(2, at=c(20,25,30))
 legend('topright', col=c('green','blue'), pch=16, legend=c('female','male'), bty='n', cex=0.5)
 segments(x0=0.75,x1=1.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
 segments(x0=1.75,x1=2.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-polygon(x=c(0,5,5,0), y=c(23.9,23.9,27.1,27.1), col=rgb(0,0,1,0.2), border=NA)
-polygon(x=c(0,5,5,0), y=c(25.2,25.2,27.1,27.1), col=rgb(1,0,0,0.2), border=NA)
-segments(x0=c(0), x1=c(5), y0=c(25.6), y1=c(25.6), lty=3, col=rgb(0,0,1))
-segments(x0=c(0), x1=c(5), y0=c(26.1), y1=c(26.1), lty=3, col=rgb(1,0,0))
 
 plot(f_res ~ jitter(as.numeric(sex), 0.5), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='f (Hz)', xlab='sex', xaxt='n', xlim=c(0.5,2.5), ylim=c(19,33), cex=0.5, yaxt='n')
-axis(1, at=c(1,2), labels=c('male','female'))
+axis(1, at=c(1,2), labels=c('female','male'))
 axis(2, at=c(20,25,30))
-segments(x0=0.75,x1=1.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-segments(x0=1.75,x1=2.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=0.75,x1=1.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=1.75,x1=2.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+
+polygon(x=c(0,5,5,0), y=c(22.2,22.2,28.5,28.5), col=rgb(0,0,0,0.2), border=NA)
+segments(x0=c(0), x1=c(5), y0=c(25.6), y1=c(25.6), lty=3, col=rgb(0,0,1))
+segments(x0=c(0), x1=c(5), y0=c(26.1), y1=c(26.1), lty=3, col=rgb(1,0,0))
 
 plot(f_res ~ jitter(width,2), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='f (Hz)', xlab='width (cm)', ylim=c(19,33), cex=0.5, yaxt='n', xaxt='n')
 axis(2, at=c(20,25,30))
@@ -128,10 +132,10 @@ segments(x0=0.75,x1=1.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('
 segments(x0=1.75,x1=2.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
 
 plot(q ~ jitter(as.numeric(sex), 0.5), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='Q', xlab='sex', xaxt='n', xlim=c(0.5,2.5), ylim=c(2,9), cex=0.5, yaxt='n')
-axis(1, at=c(1,2), labels=c('male','female'))
+axis(1, at=c(1,2), labels=c('female','male'))
 axis(2, at=c(3,6,9))
-segments(x0=0.75,x1=1.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-segments(x0=1.75,x1=2.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=0.75,x1=1.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=1.75,x1=2.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
 
 plot(q ~ jitter(width,2), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='Q', xlab='width (cm)', ylim=c(2,9), cex=0.5, yaxt='n', xaxt='n')
 axis(2, at=c(3,6,9))
@@ -146,5 +150,6 @@ plot(q ~ jitter(top_area, 3), subset(vib, whole_single=='whole'), pch=16, col=se
 axis(2, at=c(3,6,9))
 axis(1, at=c(4,6.5,9))
 
-# male mean 25.6, range of bird means 23.9-27.1
-# female mean 26.1, range of bird means 25.2-27.1
+dev.off()
+
+
