1dbc0d049267c8a498f81027a8dfaeb68213b982
rozdakin <roslyn.dakin@gmail.com>
2017-09-08 13:37:48 -0400

    added other morph into analysis, redraw figs

--- peacock_crest_biomechanics.R
+++ peacock_crest_biomechanics.R
@@ -35,7 +35,8 @@ vib$sweep <- factor(ifelse(vib$whole_single=='whole', vib$exp, NA))
 summary(vib$sweep)
 
 summary(vib)
-vib$orientation <- factor(vib$orient, levels=c('standard','sideways')) # ask Dan to verify anatomical reference for the two orientations
+vib$orientation <- factor(vib$orient, levels=c('standard','sideways')) # change to anatomical reference for the two orientations
+vib$orientation <- factor(ifelse(vib$orient=='standard','A_out-plane','B_in-plane'))
 vib$sex <- factor(vib$sex, levels=c('female','male'))
 vib$sex_col <- ifelse(vib$sex=='female', 'green', 'blue')
 
@@ -45,38 +46,53 @@ vib$q <- vib$f_res/vib$del_f
 # whole crest
 
 # quality factor.
-mod.q <- lme(q ~ orientation + sweep + sex + width + height + top_area + nfeathers + run, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
+mod.q <- lme(q ~ orientation + sex + sweep + run, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
 summary(mod.q)
 # first we check run and sweep. both ns therefore remove those parameters
-mod.q <- lme(q ~ orientation + sex + width + height + top_area + nfeathers, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
-summary(mod.q)
-dev.new(); plot(mod.q) # fine, no outliers
-dev.new(); hist(residuals(mod.q)) # good
+# next we assess morphology. with only 15 crests, we consider only one trait at a time
+mod.q <- lme(q ~ orientation + sex, random=~1|crest_number, data=subset(vib, whole_single=='whole'&!is.na(nfeathers)), method='ML') # no trait model
+mod.q1 <- update(mod.q, .~.+top_area)
+mod.q2 <- update(mod.q, .~.+width)
+mod.q3 <- update(mod.q, .~.+height)
+mod.q4 <- update(mod.q, .~.+nfeathers)
+mod.q5 <- update(mod.q, .~.+percent_unalign)
+mod.q6 <- update(mod.q, .~.+percent_short)
+AICc(mod.q1, mod.q2, mod.q3, mod.q4, mod.q5, mod.q6) # mod.q1 is lowest
+mod.q1 <- update(mod.q1, method='REML')
+summary(mod.q1)
+dev.new(); plot(mod.q1) # fine, no outliers
+dev.new(); hist(residuals(mod.q1)) # good
 
 png(file='./figures/Qmod_resid_plot.png', width=4, height=5, res=300, units='in', bg='white')
-par(mfrow=c(3,2), bty='l', mar=c(4,4,0.25,0.25), mgp=c(1.5,0.5,0)); visreg(mod.q, cond=list(orientation='standard'), ylab='Q', ylim=c(3,9))
+par(mfrow=c(3,2), bty='l', mar=c(4,4,0.25,0.25), mgp=c(1.5,0.5,0)); visreg(mod.q1, cond=list(orientation='A_out-plane'), ylab='Q', ylim=c(3,9))
 dev.off()
 
-r.squaredGLMM(mod.q) # 46% var explained in Q by fixed effects, very high
+r.squaredGLMM(mod.q1) # 49% var explained in Q by fixed effects, fairly high
 mod.q.rep <- lme(q ~ 1, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
 r.squaredGLMM(mod.q.rep) # 50% measurement repeatability
 
 # resonant freq.
-mod.f <- lme(f_res ~ orientation + sweep + sex + width + height + top_area + nfeathers + run, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
+mod.f <- lme(f_res ~ orientation + sex + sweep + run, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
 summary(mod.f) # first we check run and sweep. both ns therefore eliminate. residuals indicate that variance differs among the crests, so we furthermore use a model with different variances:
-mod.f <- lme(f_res ~ orientation + sex + width + height + top_area + nfeathers, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit, weights=varIdent(form=~1|crest_number))
-summary(mod.f)
-dev.new(); plot(mod.f) # good
-dev.new(); hist(residuals(mod.f)) # ok
+mod.f <- lme(f_res ~ orientation + sex, random=~1|crest_number, data=subset(vib, whole_single=='whole'&!is.na(nfeathers)), weights=varIdent(form=~1|crest_number), method='ML') # no trait model
+mod.f1 <- update(mod.f, .~.+top_area)
+mod.f2 <- update(mod.f, .~.+width)
+mod.f3 <- update(mod.f, .~.+height)
+mod.f4 <- update(mod.f, .~.+nfeathers)
+mod.f5 <- update(mod.f, .~.+percent_unalign)
+mod.f6 <- update(mod.f, .~.+percent_short)
+AICc(mod.f1, mod.f2, mod.f3, mod.f4, mod.f5, mod.f6) # mod.f1 is lowest
+mod.f1 <- update(mod.f1, method='REML')
+summary(mod.f1)
+dev.new(); plot(mod.f1) # good
+dev.new(); hist(residuals(mod.f1)) # ok
 
 png(file='./figures/fmod_resid_plot.png', width=4, height=5, res=300, units='in', bg='white')
-par(mfrow=c(3,2), bty='l', mar=c(4,4,0.25,0.25), mgp=c(1.5,0.5,0)); visreg(mod.f, cond=list(orientation='standard'), ylab='f_res', ylim=c(19,33))
+par(mfrow=c(3,2), bty='l', mar=c(4,4,0.25,0.25), mgp=c(1.5,0.5,0)); visreg(mod.f1, cond=list(orientation='A_out-plane'), ylab='f_res', ylim=c(19,33))
 dev.off()
-# the more top area, the lower the f_res. b/c more massive, perhaps? confounded with sex
-# ns. positive effect of n feathers, width. ns. negative effect of height
-# higher in standard orientation
+# higher in standard orientation, larger top area, tend to have lower res. freq
 
-r.squaredGLMM(mod.f) # 34% ver explained in f res. by fixed effects, high
+r.squaredGLMM(mod.f1) # 28% ver explained in f res. by fixed effects
 mod.f.rep <- lme(f_res ~ 1, random=~1|crest_number, data=subset(vib, whole_single=='whole'), na.action=na.omit)
 r.squaredGLMM(mod.f.rep) # 82% measurement repeatability, very high
 
@@ -94,18 +110,23 @@ plot(f_res ~ jitter(as.numeric(orientation), 0.5), subset(vib, whole_single=='wh
 axis(1, at=c(1,2), labels=c('standard','sideways'))
 axis(2, at=c(20,25,30))
 legend('topright', col=c('green','blue'), pch=16, legend=c('female','male'), bty='n', cex=0.5)
-segments(x0=0.75,x1=1.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-segments(x0=1.75,x1=2.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=0.75,x1=1.25, y0=mean(subset(vib, whole_single=='whole'&orientation=='A_out-plane')$f_res))
+segments(x0=1.75,x1=2.25, y0=mean(subset(vib, whole_single=='whole'&orientation=='B_in-plane')$f_res))
 
 plot(f_res ~ jitter(as.numeric(sex), 0.5), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='f (Hz)', xlab='sex', xaxt='n', xlim=c(0.5,2.5), ylim=c(19,33), cex=0.5, yaxt='n')
 axis(1, at=c(1,2), labels=c('female','male'))
 axis(2, at=c(20,25,30))
-segments(x0=0.75,x1=1.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-segments(x0=1.75,x1=2.25, y0=((predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.f, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=0.75,x1=1.25, y0=mean(subset(vib, whole_single=='whole'&sex=='female')$f_res))
+segments(x0=1.75,x1=2.25, y0=mean(subset(vib, whole_single=='whole'&sex=='male')$f_res))
 
-polygon(x=c(0,5,5,0), y=c(22.2,22.2,28.5,28.5), col=rgb(0,0,0,0.2), border=NA) # range of frequencies recorded over all shaking displays 
+polygon(x=c(0,5,5,0), y=c(22.2,22.2,28.5,28.5), col=rgb(0,0,0,0.2), border=NA) # range of freq for all shaking displays 
 segments(x0=c(0), x1=c(5), y0=c(25.6), y1=c(25.6), lty=3, col=rgb(0,0,1)) # peacock shaking display shaking avg
 segments(x0=c(0), x1=c(5), y0=c(26.1), y1=c(26.1), lty=3, col=rgb(1,0,0)) # peahen shaking display avg
+
+plot(f_res ~ jitter(top_area, 3), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', xlab='top area (cm2)', ylab='f (Hz)', ylim=c(19,33), cex=0.5, yaxt='n', xaxt='n')
+axis(2, at=c(20,25,30))
+axis(1, at=c(4,6.5,9))
+# sex effect is partially due to top area?
 
 plot(f_res ~ jitter(width,2), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='f (Hz)', xlab='width (cm)', ylim=c(19,33), cex=0.5, yaxt='n', xaxt='n')
 axis(2, at=c(20,25,30))
@@ -119,24 +140,21 @@ plot(f_res ~ jitter(nfeathers,1), subset(vib, whole_single=='whole'), pch=16, co
 axis(2, at=c(20,25,30))
 axis(1, at=c(20,25,30))
 
-plot(f_res ~ jitter(top_area, 3), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', xlab='top area (cm2)', ylab='f (Hz)', ylim=c(19,33), cex=0.5, yaxt='n', xaxt='n')
-axis(2, at=c(20,25,30))
-axis(1, at=c(4,6.5,9))
-segments(x0=c(4), x1=c(9), y0=predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=4, nfeathers=median(morph$nfeathers, na.rm=T)), level=0), y1=predict(mod.f, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=9, nfeathers=median(morph$nfeathers, na.rm=T)), level=0), lty=3)
-# sex effect is partially due to top area? one or the other is significant
-
 plot(q ~ jitter(as.numeric(orientation), 0.5), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='Q', xlab='orientation', xaxt='n', xlim=c(0.5,2.5), ylim=c(2,9), cex=0.5, yaxt='n')
 axis(1, at=c(1,2), labels=c('standard','sideways'))
 axis(2, at=c(3,6,9))
-segments(x0=0.75,x1=1.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-segments(x0=1.75,x1=2.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=0.75,x1=1.25, y0=mean(subset(vib, whole_single=='whole'&orientation=='A_out-plane')$q))
+segments(x0=1.75,x1=2.25, y0=mean(subset(vib, whole_single=='whole'&orientation=='B_in-plane')$q))
 
 plot(q ~ jitter(as.numeric(sex), 0.5), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='Q', xlab='sex', xaxt='n', xlim=c(0.5,2.5), ylim=c(2,9), cex=0.5, yaxt='n')
 axis(1, at=c(1,2), labels=c('female','male'))
 axis(2, at=c(3,6,9))
-segments(x0=0.75,x1=1.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='female',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
-segments(x0=1.75,x1=2.25, y0=((predict(mod.q, newdata=data.frame(orientation=c('standard'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)) + (predict(mod.q, newdata=data.frame(orientation=c('sideways'), sex='male',width=median(morph$width), height=median(morph$height), top_area=median(morph$top_area), nfeathers=median(morph$nfeathers, na.rm=T)), level=0)))/2)
+segments(x0=0.75,x1=1.25, y0=mean(subset(vib, whole_single=='whole'&sex=='female')$q))
+segments(x0=1.75,x1=2.25, y0=mean(subset(vib, whole_single=='whole'&sex=='male')$q))
 
+plot(q ~ jitter(top_area, 3), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', xlab='top area (cm2)', ylab='Q', ylim=c(2,9), cex=0.5, yaxt='n',xaxt='n')
+axis(2, at=c(3,6,9))
+axis(1, at=c(4,6.5,9))
 plot(q ~ jitter(width,2), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='Q', xlab='width (cm)', ylim=c(2,9), cex=0.5, yaxt='n', xaxt='n')
 axis(2, at=c(3,6,9))
 axis(1, at=c(4,5.5,7))
@@ -146,9 +164,6 @@ axis(1, at=c(4.5,5.5,6.5))
 plot(q ~ jitter(nfeathers,1), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', ylab='Q', xlab='# feathers', ylim=c(2,9), cex=0.5, yaxt='n', xaxt='n')
 axis(2, at=c(3,6,9))
 axis(1, at=c(20,25,30))
-plot(q ~ jitter(top_area, 3), subset(vib, whole_single=='whole'), pch=16, col=sex_col, bty='l', xlab='top area (cm2)', ylab='Q', ylim=c(2,9), cex=0.5, yaxt='n',xaxt='n')
-axis(2, at=c(3,6,9))
-axis(1, at=c(4,6.5,9))
 
 dev.off()
 
